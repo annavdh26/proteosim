48bac99bdac5f9cc6e4f471b3b179dc6404ffe3c
annavdh26 <avdh3@icloud.com>
2025-11-20 16:58:05 +0100

    test

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -3,7 +3,7 @@ from proteosim.file_handling import read_fasta
 
 
 def test_fasta_reader():
-    tmp_fasta_path = 'data\\dummy-proteins.fasta'
+    tmp_fasta_path = 'data/dummy-proteins.fasta'
     # Read the file contents into a single string named `raw_fasta_text`.
     protein_mapg =read_fasta(tmp_fasta_path)
     # Replace the strings with your fasta content
